986f7291c5e946cbcd3eede90d9d93a9a4416cd3
mqgmaster <mqgmaster@gmail.com>
2014-10-29 17:09:58 -0200

    projeto renomeado #2

---
+++ condominium-manager/src/main/java/br/ufrgs/inf/gar/snmp/condominium/manager/ui/tab/condominium/CondominiumInfoTab.java
@@ -0,0 +1,11 @@
+package br.ufrgs.inf.gar.snmp.condominium.manager.ui.tab.condominium;
+
+import br.ufrgs.inf.gar.snmp.condominium.manager.ui.tab.generic.AbstractTab;
+
+@SuppressWarnings("serial")
+public class CondominiumInfoTab extends AbstractTab<CondominiumInfoLayout>{
+
+	public CondominiumInfoTab() {
+		super(CondominiumInfoLayout.class);
+	}
+}
---
+++ condominium-manager/src/main/java/br/ufrgs/inf/gar/snmp/condominium/manager/ui/tab/home/HomeTab.java
@@ -0,0 +1,34 @@
+package br.ufrgs.inf.gar.snmp.condominium.manager.ui.tab.home;
+
+import java.io.IOException;
+
+import br.ufrgs.inf.gar.snmp.condominium.manager.service.SNMPManager;
+import br.ufrgs.inf.gar.snmp.condominium.manager.ui.tab.generic.AbstractTab;
+
+import com.vaadin.ui.Button;
+import com.vaadin.ui.Button.ClickEvent;
+
+@SuppressWarnings("serial")
+public class HomeTab extends AbstractTab<HomeLayout> {
+	
+	public HomeTab() {
+		super(HomeLayout.class);
+		
+		/**
+		* OID - .1.3.6.1.2.1.1.4.0 => sysContact
+		* => MIB explorer/ browser é util para confirmar isso
+		*/
+		layout.getOidField().setValue(".1.3.6.1.2.1.1.4.0");
+		layout.getOidButton().addClickListener(new Button.ClickListener() {
+	        public void buttonClick(ClickEvent event) {
+	            try {
+	            	layout.getResponseLabel().setValue(
+	            			SNMPManager.getAsString(layout.getOidField().getValue()));
+				} catch (IOException e) {
+					layout.getResponseLabel().setValue("error?");
+					e.printStackTrace();
+				}
+	        }
+	    });
+	}
+}
