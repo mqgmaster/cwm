00fdb9b8012ea9c67575aec61f5cbff444a14900
mqgmaster <mqgmaster@gmail.com>
2014-11-24 18:39:28 -0200

    testes com tabelas

--- condo-web-manager/src/main/java/br/ufrgs/inf/gar/cwm/dash/condo/EmployeeTable.java
+++ condo-web-manager/src/main/java/br/ufrgs/inf/gar/cwm/dash/condo/EmployeeTable.java
@@ -3,9 +3,14 @@ package br.ufrgs.inf.gar.cwm.dash.condo;
 import br.ufrgs.inf.gar.condo.domain.Employee;
 import br.ufrgs.inf.gar.cwm.dash.data.DaoService;
 
+import com.vaadin.data.Container;
 import com.vaadin.data.Property;
 import com.vaadin.data.util.BeanItemContainer;
+import com.vaadin.ui.Component;
+import com.vaadin.ui.Field;
 import com.vaadin.ui.Table;
+import com.vaadin.ui.TableFieldFactory;
+import com.vaadin.ui.TextField;
 import com.vaadin.ui.themes.ValoTheme;
 
 @SuppressWarnings("serial")
@@ -37,6 +42,7 @@ public final class EmployeeTable extends Table {
         addStyleName(ValoTheme.TABLE_NO_VERTICAL_LINES);
         addStyleName(ValoTheme.TABLE_SMALL);
         setSortEnabled(false);
+        setImmediate(true);
         setSizeFull();
 
         BeanItemContainer<Employee> container = new BeanItemContainer<>(
@@ -44,6 +50,17 @@ public final class EmployeeTable extends Table {
         setContainerDataSource(container);
         setVisibleColumns(new Object[]{"name", "role", "monthWage", "weekWorkload", "working"});
         setColumnHeaders("Nome", "Cargo", "Salário", "Carga Horária", "Status");
+        
+        this.setTableFieldFactory(new TableFieldFactory() {
+			@Override
+			public Field<?> createField(Container container, Object itemId,
+					Object propertyId, Component uiContext) {
+				TextField field = new TextField((String) propertyId);
+                if ("working".equals(propertyId)) {
+                    field.setImmediate(true);
+                }
+                return field;
+			}
+		});
     }
-
 }
